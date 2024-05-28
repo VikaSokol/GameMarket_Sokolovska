dc2eb8a04261d88140c19a0a711e82d1016c185f
VikaSokol <148072910+VikaSokol@users.noreply.github.com>
2024-05-28 23:47:16 +0300

    ready

--- final/final.js
+++ final/final.js
@@ -75,69 +75,253 @@
     // },
 //]
 
+
+
+
+console.log('Перевірка підключеного файлу скрипту market.js')
 let itemsArray = [
   {
-    email: "23b_lam@liceum.ztu.edu.ua",
-    class: "10-Б 2",
-    name: "Лещенко Настя",
-    nickname: "dude настюшкі",
-    author_photo: "Лещенко.jpg",
-    game_photo: "Лещенко_game.jpg",
-    github_project1: "https://github.com/Leschenko1001/Phaser2ndGame1?authuser=0",
-    project1_demo: "https://leschenko1001.github.io/Phaser2ndGame1/?authuser=0",
-    google_drive: "https://drive.google.com/drive/folders/1CqBXUS5Rk1u8nhEq3_l7LYMjDalYqPgW?usp=classroom_web&authuser=0",
-    google_form: "https://forms.gle/aLmXJ7nEnjo6PGuZ8?authuser=0",
-    github_project2: "https://github.com/Leschenko1001/GameMarket_Leschenko",
-    project2_demo: "https://leschenko1001.github.io/GameMarket_Leschenko/?authuser=0",
-    project2_market: "https://leschenko1001.github.io/GameMarket_Leschenko/market.html?authuser=0",
-    project2_final: "https://leschenko1001.github.io/GameMarket_Leschenko/final/final.html?authuser=0"
-  }
+      "email": "23b_lam@liceum.ztu.edu.ua",
+      "group": "10-Б 2",
+      "name": "Лещенко Настя",
+      "project_name": "dude настюшкі",
+      "project_github": "https://github.com/Leschenko1001/Phaser2ndGame1?authuser=0",
+      "project_demo": "https://leschenko1001.github.io/Phaser2ndGame1/?authuser=0",
+      "project_drive": "https://drive.google.com/drive/folders/1CqBXUS5Rk1u8nhEq3_l7LYMjDalYqPgW?usp=classroom_web&authuser=0",
+      "project_form": "https://forms.gle/aLmXJ7nEnjo6PGuZ8?authuser=0",
+      "gamemarket_github": "https://github.com/Leschenko1001/GameMarket_Leschenko",
+      "gamemarket_demo": "https://leschenko1001.github.io/GameMarket_Leschenko/?authuser=0",
+      "gamemarket_market_page": "https://leschenko1001.github.io/GameMarket_Leschenko/market.html?authuser=0",
+      "gamemarket_final_page": "https://leschenko1001.github.io/GameMarket_Leschenko/final/final.html?authuser=0",
+      "img_author": "Лещенко.jpg",
+      "img_game": "Лещенко_game.jpg"
+  },
+  {
+    "email": "23b_lsa@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Лобода Софія",
+    "project_name": "MOKOSH",
+    "project_github": "https://github.com/Lobodass/project.git",
+    "project_demo": "https://lobodass.github.io/project/",
+    "project_drive": "https://drive.google.com/drive/folders/1-0efrhem62Vc__8Ir6qIFuatZJ_FjV50?usp=classroom_web&authuser=0",
+    "project_form": "https://forms.gle/5DBw7bJ5v33uCqbt5",
+    "gamemarket_github": "https://github.com/Lobodass/siteinf.git",
+    "gamemarket_demo": "https://lobodass.github.io/siteinf/",
+    "gamemarket_market_page": "https://lobodass.github.io/siteinf/marcet.html?authuser=0",
+    "gamemarket_final_page": "https://lobodass.github.io/siteinf/final/final.html?authuser=0",
+    "img_author": "Лобода_game.jpg",
+    "img_game": "Лобода.jpg"
+  },
+  {
+    "email": "23b_nar@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Найбородін Артем",
+    "project_name": "BestGame",
+    "project_github": "https://github.com/Nayborodin/MyGame",
+    "project_demo": "https://nayborodin.github.io/MyGame/",
+    "project_drive": "https://drive.google.com/drive/folders/1G102lDz-nuYjqtOjxfLFwW2zB1R3BCgE",
+    "project_form": "https://forms.gle/vitg4Rk5EUm3MZe38",
+    "gamemarket_github": "https://github.com/Nayborodin/GameMarket_Nayborodin",
+    "gamemarket_demo": "https://nayborodin.github.io/GameMarket_Nayborodin/",
+    "gamemarket_market_page": "https://nayborodin.github.io/GameMarket_Nayborodin/market.html",
+    "gamemarket_final_page": "https://nayborodin.github.io/GameMarket_Nayborodin/final/final.html",
+    "img_author": "Найбородін.jpg",
+    "img_game": "Найбородін_game.jpg"
+  },
+{
+    "email": "23b_nod@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Нікітіч Олександр",
+    "project_name": "",
+    "project_github": "https://github.com/SashaNikitich/Phaser2ndGame-2",
+    "project_demo": "https://sashanikitich.github.io/Phaser2ndGame-2/",
+    "project_drive": "",
+    "project_form": "https://docs.google.com/forms/d/e/1FAIpQLSdfx9mrL6L9s8P2Ratdn_TG0xqCTmyDBL3GfhUYM_27OnUmFA/viewform?usp=sharing",
+    "gamemarket_github": "https://github.com/SashaNikitich/Phaser2ndGame-2",
+    "gamemarket_demo": "https://sashanikitich.github.io/Phaser2ndGame-2/",
+    "gamemarket_market_page": "",
+    "gamemarket_final_page": "",
+    "img_author": "Нікітіч.jpg",
+    "img_game": "Нікітіч_game.jpg"
+},
+{
+    "email": "23b_pos@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Продоус Олександр",
+    "project_name": "SuperGame",
+    "project_github": "https://github.com/Oleksandrpr321/-Phaser2ndGame.git",
+    "project_demo": "https://oleksandrpr321.github.io/-Phaser2ndGame/",
+    "project_drive": "https://drive.google.com/drive/folders/15dRiiBT-A6CAsZa0mXBR7k9_2edGz6bP?usp=drive_link",
+    "project_form": "https://forms.gle/Y3SEVntm9Tr7znXu9",
+    "gamemarket_github": "https://github.com/Oleksandrpr321/-GameMarket_Prodous.git",
+    "gamemarket_demo": "https://oleksandrpr321.github.io/-GameMarket_Prodous/",
+    "gamemarket_market_page": "https://oleksandrpr321.github.io/-GameMarket_Prodous/market.html",
+    "gamemarket_final_page": "https://oleksandrpr321.github.io/-GameMarket_Prodous/final/final.html",
+    "img_author": "Продоус.jpg",
+    "img_game": "Продоус_game.jpg"
+  },
+{
+    "email": "23b_pds@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Прокопчук Денис",
+    "project_name": "Апокаліпсіс",
+    "project_github": "https://github.com/Denis16543/Phaser2ndGame",
+    "project_demo": "https://denis16543.github.io/Phaser2ndGame/",
+    "project_drive": "https://drive.google.com/drive/folders/1KKIoePi4fkPjpWY4mAYEOL99wByIXX_t?usp=drive_link",
+    "project_form": "https://forms.gle/yn4n8Gsu67vAtx8S9",
+    "gamemarket_github": "https://github.com/Denis16543/GameMarket",
+    "gamemarket_demo": "https://denis16543.github.io/GameMarket/",
+    "gamemarket_market_page": "https://denis16543.github.io/GameMarket_Prokopchuk/",
+    "gamemarket_final_page": "https://denis16543.github.io/GameMarket_Prokopchuk/final/final.html",
+    "img_author": "Прокопчук.jpg",
+    "img_game": "Прокопчук_game.jpg"
+  },
+{
+    "email": "23b_syei@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Савицкьий Єгор",
+    "project_name": "Falcon",
+    "project_github": "https://github.com/HellcatFalcon/game.git",
+    "project_demo": "https://github.com/HellcatFalcon/Faclon/settings/pages",
+    "project_drive": "https://drive.google.com/drive/folders/1yCCzefMmd5BGGz9UBrljEXQWQThK9ImN?usp=drive_link",
+    "project_form": "https://forms.gle/jAf9YW87aQqeoj4v6",
+    "gamemarket_github": "https://github.com/HellcatFalcon/GameMarket_Savitskiy.git",
+    "gamemarket_demo": "https://hellcatfalcon.github.io/GameMarket_Savitskiy/",
+    "gamemarket_market_page": "http://127.0.0.1:5500/market.html",
+    "gamemarket_final_page": "http://127.0.0.1:5500/final/final.html",
+    "img_author": "Савицький.jpg",
+    "img_game": "Савицький_game.jpg"
+},
+{
+    "email": "23b_stv@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Саух Тимофій",
+    "project_name": "якісь колодязі",
+    "project_github": "https://github.com/imnottimaw/Phaser2ndGame",
+    "project_demo": "https://imnottimaw.github.io/Phaser2ndGame/",
+    "project_drive": "https://drive.google.com/drive/folders/1YvPNGVqCD6BBzWr4azgHLxwar91rtT6H?usp=sharing",
+    "project_form": "https://forms.gle/a58ghZiZdVJfZVLg6",
+    "gamemarket_github": "https://github.com/imnottimaw/GameMarket_Saukh",
+    "gamemarket_demo": "https://imnottimaw.github.io/GameMarket_Saukh/",
+    "gamemarket_market_page": "https://imnottimaw.github.io/GameMarket_Saukh/market.html",
+    "gamemarket_final_page": "https://imnottimaw.github.io/GameMarket_Saukh/final/final.html",
+    "img_author": "Саух.jpg",
+    "img_game": "Саух_game.jpg"
+},
+{
+    "email": "23b_som@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Скримінська Оксана",
+    "project_name": "",
+    "project_github": "https://github.com/skryminska/Phaser2ndGame",
+    "project_demo": "https://skryminska.github.io/Phaser2ndGame/",
+    "project_drive": "https://docs.google.com/document/d/19B0k9awxwUCrPf3vL68ekIFYvMzU3q-tIg8Q3hMjbHQ/edit?usp=classroom_web&authuser=0",
+    "project_form": "",
+    "gamemarket_github": "https://github.com/skryminska/GameMarket_Skryminska",
+    "gamemarket_demo": "https://skryminska.github.io/GameMarket_Skryminska/",
+    "gamemarket_market_page": "",
+    "gamemarket_final_page": "",
+    "img_author": "Скримінська.jpg",
+    "img_game": "Скримінська_game.jpg"
+},
+{
+    "email": "23b_sns@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Смолянова Ніка",
+    "project_name": "Exiting way",
+    "project_github": "https://github.com/Nikasms/-Phaser2ndGame.git",
+    "project_demo": "https://nikasms.github.io/-Phaser2ndGame/",
+    "project_drive": "https://drive.google.com/drive/folders/1oNBngdHeURy6zi-iwPPfUc8XsuIx5Ekk",
+    "project_form": "https://forms.gle/MU9K5RhTwm4SJAXeA",
+    "gamemarket_github": "https://github.com/Nikasms/GameMarket_Smolianova.git",
+    "gamemarket_demo": "https://nikasms.github.io/GameMarket_Smolianova/",
+    "gamemarket_market_page": "https://nikasms.github.io/GameMarket_Smolianova/market.html",
+    "gamemarket_final_page": "https://nikasms.github.io/GameMarket_Smolianova/final/final.html",
+    "img_author": "Смолянова.jpg",
+    "img_game": "Смолянова_game.jpg"
+},
+{
+    "email": "23b_tdp@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Терський Дмитро",
+    "project_name": "",
+    "project_github": "https://github.com/dmitro321w/GameMarket_-/tree/main",
+    "project_demo": "",
+    "project_drive": "https://drive.google.com/drive/folders/1izA5dAz4VlfRCffwQCXKGeLpKX3qpDGA?usp=drive_link",
+    "project_form": "",
+    "gamemarket_github": "https://github.com/dmitro321w/GameMarket_-/tree/main",
+    "gamemarket_demo": "https://dmitro321w.github.io/GameMarket_-/",
+    "gamemarket_market_page": "",
+    "gamemarket_final_page": "",
+    "img_author": "Терський.jpg",
+    "img_game": "Терський_game.jpg"
+},
+{
+    "email": "23b_shmv@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Шапіренко Максим",
+    "project_name": "Roborun",
+    "project_github": "https://github.com/Shapirenko/Roborun",
+    "project_demo": "https://shapirenko.github.io/Roborun/",
+    "project_drive": "https://drive.google.com/drive/folders/1j-uM3IKdJUH3IbqpNKxAvkDJvP_XrunA?usp=classroom_web&authuser=0",
+    "project_form": "https://forms.gle/Ty19Ttoo6CWEGdp78?authuser=0",
+    "gamemarket_github": "https://github.com/Shapirenko/GameMarket_Shapirenko?authuser=0",
+    "gamemarket_demo": "https://shapirenko.github.io/GameMarket_Shapirenko/?authuser=0",
+    "gamemarket_market_page": "https://shapirenko.github.io/GameMarket_Shapirenko/market.html?hl=ru&authuser=0",
+    "gamemarket_final_page": "https://shapirenko.github.io/GameMarket_Shapirenko/final/final.html?authuser=0",
+    "img_author": "Шапіренко.jpg",
+    "img_game": "Шапіренко_game.jpg"
+},
+{
+    "email": "23b_svyu@liceum.ztu.edu.ua",
+    "group": "10-Б 2",
+    "name": "Соколовська Вікторія",
+    "project_name": "Космічне сміття",
+    "project_github": "https://github.com/VikaSokol/Space-Trash-",
+    "project_demo": "https://vikasokol.github.io/Space-Trash-/",
+    "project_drive": "https://drive.google.com/drive/folders/10W9NbLTXlXtVCFWZfdoyVVfpVku7qJwA?usp=drive_link",
+    "project_form": "",
+    "gamemarket_github": "https://github.com/VikaSokol/GameMarket_Sokolovska",
+    "gamemarket_demo": "https://vikasokol.github.io/Space-Trash-/",
+    "gamemarket_market_page": "https://vikasokol.github.io/market/market.html?authuser=0",
+    "gamemarket_final_page": "https://vikasokol.github.io/market/final/final.html?authuser=0",
+    "img_author": "Соколовська.jpg",
+    "img_game": "Соколовська_game.jpg"
+}
 ]
 
-//отримання елементу з ідентифікатором items
-let itemsDiv = document.getElementById("items")
 
-//перевірка існування знайденого блоку
+
+
+
+
+
+
+let itemsDiv = document.getElementById("items");
+
 if (itemsDiv) {
-    //створення блоків по кількості елементів 
-    itemsArray.forEach((item, index) =>{
-        // console.log(item)
-        itemsDiv.innerHTML += 
-        `
-        <div class="item"> 
-
-        <!-- блок назва товару  -->
-        <div class="item-title">${item.nickname}</div>
-        <div class="item-title">${item.class}</div>
-        <div class="item-title">${item.email}</div>
-
-        <!-- блок зображення товару  -->
-        <div class="item-image">
-          <img src="img/${item.author_photo}" class="image" />
-          <img src="img/${item.game_photo}" class="image" />
-        </div>
-
-        <!-- блок з оплатою частинами -->
-        <div class="parts-pay">
-          <div><img src="photo/mono.png" alt="">${item.mono}</div>
-          <div><img src="photo/pb.jpg" alt="">${item.pb}</div>
-        </div>
-        <!-- блок ціни -->
-        <div class="price">
-          <div><span>${item.prise_standart}</span> <sup>грн</sup></div>
-          <div><span>${item.prise_discount}</span> <sup>грн</sup></div>
-        </div>
-
-        <!-- блок бонусної ціни  -->
-        <div class="price bonus">
-          <div>ціна за купоном</div>
-          <div><span>${item.prise_coupon}</span> <sup>грн</sup></div>
-        </div>
-      </div>
-      
-        `
-})
+  itemsArray.forEach(item => {
+      itemsDiv.innerHTML += 
+      `
+      <div class="item">
+          <div class="item-title">${item.project_name}</div>
+          <p>${item.name} ${item.group} ${item.email}</p>
+          <div class="content">
+              <img src="img/${item.img_game}" alt="Game Image">
+              <img src="img/${item.img_author}" alt="Author Image">
+          </div>
+          <div class="links">
+              <a href="${item.project_github}" target="_blank">GitHub Repository</a>
+              <a href="${item.project_demo}" target="_blank">GitHub Pages Game</a>
+              <a href="${item.project_drive}" target="_blank">Google Drive Documentation</a>
+              <a href="${item.project_form}" target="_blank">Survey Form</a>
+              <a href="${item.gamemarket_github}" target="_blank">GitHub Repository GameMarket</a>
+              <a href="${item.gamemarket_demo}" target="_blank">GitHub Pages Design</a>
+              <a href="${item.gamemarket_market_page}" target="_blank">Market Pages 4 Products</a>
+              <a href="${item.gamemarket_final_page}" target="_blank">Market Pages Final</a>
+          </div>
+      </div>`;
+  });
 } else {
-    //вивід повідомлення про не знайдений блок
-    console.log('блок товарів не знайдено')
+  console.log('Блок товарів не знайдено');
 }
